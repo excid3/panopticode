af006aa1319336fe64bb4635f27432a9625e9e3f
Chris Oliver <excid3@gmail.com>
2011-11-03 19:41:24 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Panopticode::Application.config.secret_token = '34f5850552f994e86dfe55511be0e29bcc6b73b16053183c3b44b9102dbb9e385c0b4f587e0a3a353d93e80418132496b735630e2028bc6e846e1799da9ad242'
